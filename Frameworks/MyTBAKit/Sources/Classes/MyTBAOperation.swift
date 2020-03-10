0fbc1208d3e756cc22e6f9349126e5628ea2f6d4
Zachary Orr <ZachOrr@users.noreply.github.com>
2020-03-10 14:07:13 -0400

    Still not good

--- Frameworks/MyTBAKit/Sources/Classes/MyTBAOperation.swift
+++ Frameworks/MyTBAKit/Sources/Classes/MyTBAOperation.swift
@@ -1,7 +1,63 @@
 import Foundation
 import TBAOperation
 
+// TODO: Move this out
+protocol MyTBAUser {
+    func getToken(completion: ((String?, Error) -> ()?))
+}
+
 public class MyTBAOperation: TBAOperation {
+
+    var tokenOperation: Operation
+    // var persistentContainerOperation: PersistentContainerOperation
+
+    let operationQueue = OperationQueue()
+
+    public init<T: MyTBAResponse>(myTBA: MyTBA, method: String, bodyData: Data? = nil, completion: @escaping (T?, Error?) -> Void) {
+        // tokenOperation = TokenOperation(user: myTBA)
+    }
+
+    /*
+    override open func execute() {
+        let blockOperation = BlockOperation { [unowned self] in
+            self.completionError = [self.destroyPersistentStoreOperation, self.persistentContainerOperation].compactMap({ $0.completionError }).first
+            self.finish()
+        }
+
+        let dependentOperations = [destroyPersistentStoreOperation, persistentContainerOperation]
+        for op in dependentOperations {
+            blockOperation.addDependency(op)
+        }
+        appSetupOperationQueue.addOperations(dependentOperations + [blockOperation], waitUntilFinished: false)
+        // task.resume()
+    }
+    */
+
+    override open func cancel() {
+        operationQueue.cancelAllOperations()
+
+        super.cancel()
+    }
+
+}
+
+private class TokenOperation: TBAOperation {
+
+    private let user: MyTBAUser
+
+    public init(user: MyTBAUser) {
+        self.user = user
+    }
+
+    override open func execute() {
+        user.getToken { (authToken, error) in
+            // Pass
+        }
+    }
+
+}
+
+private class MyTBAKitOperation: TBAOperation {
 
     var task: URLSessionTask!
 
